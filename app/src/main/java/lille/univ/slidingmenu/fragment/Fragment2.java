96c2c105fefc9db8bb7338771c34918e5d214e7f
david <david.guillon@outlook.fr>
2018-11-25 15:15:41 +0100

    create fragment class

---
+++ app/src/main/java/lille/univ/slidingmenu/fragment/Fragment2.java
@@ -0,0 +1,25 @@
+package lille.univ.slidingmenu.fragment;
+
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import lille.univ.slidingmenu.R;
+
+
+public class Fragment2 extends android.app.Fragment {
+
+    public Fragment2() {
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment2, container, false);
+            return rootView;
+    }
+
+
+}
+
---
+++ app/src/main/java/lille/univ/slidingmenu/fragment/Fragment3.java
@@ -0,0 +1,25 @@
+package lille.univ.slidingmenu.fragment;
+
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import lille.univ.slidingmenu.R;
+
+
+public class Fragment3 extends android.app.Fragment {
+
+    public Fragment3() {
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment3, container, false);
+            return rootView;
+    }
+
+
+}
+
